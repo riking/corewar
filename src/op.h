761be8d92e43c673ffb080af373ee7c1b8d6225e
Kane York <kyork@student.42.us.org>
2018-02-10 14:02:05 -0800

    op.h modification - s_header/t_header

--- src/op.h
+++ src/op.h
@@ -6,7 +6,7 @@
 /*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2018/02/10 13:59:08 by kyork            ###   ########.fr       */
+/*   Updated: 2018/02/10 14:01:42 by kyork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,12 +73,12 @@ typedef char	t_arg_type;
 # define COREWAR_EXEC_MAGIC		0xea83f3
 
 #pragma pack(4)
-typedef struct		header_s
+typedef struct		s_header
 {
 	t_u32		magic;
 	char		prog_name[PROG_NAME_LENGTH + 1];
 	t_u32		prog_size;
 	char		comment[COMMENT_LENGTH + 1];
-}					header_t;
+}					t_header;
 
 #endif
